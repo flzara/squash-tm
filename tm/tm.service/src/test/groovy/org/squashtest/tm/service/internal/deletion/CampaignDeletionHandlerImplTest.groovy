49e77f5c3dd53536a5b42c0d6def5bd029acc3a1
jthebault <jthebault@bitbucket.org>
2018-01-16 13:42:55 +0100

    fixing unit test

--- tm/tm.service/src/test/groovy/org/squashtest/tm/service/internal/deletion/CampaignDeletionHandlerImplTest.groovy
+++ tm/tm.service/src/test/groovy/org/squashtest/tm/service/internal/deletion/CampaignDeletionHandlerImplTest.groovy
@@ -20,34 +20,36 @@
  */
 package org.squashtest.tm.service.internal.deletion
 
+import org.squashtest.tm.service.attachment.AttachmentManagerService
 import org.squashtest.tm.service.campaign.IterationTestPlanManagerService;
 import org.squashtest.tm.service.internal.customfield.PrivateCustomFieldValueService
 import org.squashtest.tm.service.internal.repository.CampaignDeletionDao;
 import org.squashtest.tm.service.internal.repository.TestSuiteDao
 import  org.squashtest.tm.domain.campaign.TestSuite
-import  org.squashtest.tm.domain.campaign.IterationTestPlanItem 
+import  org.squashtest.tm.domain.campaign.IterationTestPlanItem
 import spock.lang.Specification;
 
 
 class CampaignDeletionHandlerImplTest  extends Specification{
 
-	
+
 	CampaignDeletionHandlerImpl handler = new CampaignDeletionHandlerImpl()
 	TestSuiteDao suiteDao = Mock()
 	PrivateCustomFieldValueService customValueService = Mock()
 	CampaignDeletionDao deletionDao = Mock()
 	IterationTestPlanManagerService iterationTestPlanManagerService = Mock()
-	
+	AttachmentManagerService attachmentManagerService = Mock()
+
 	def setup(){
 		handler.suiteDao = suiteDao
 		handler.customValueService = customValueService
 		handler.deletionDao = deletionDao
 		handler.iterationTestPlanManagerService = iterationTestPlanManagerService
-		
+		handler.attachmentManagerService = attachmentManagerService
 	}
-	
+
 	def "should simulate test suite deletion with no error"(){
-		
+
 		given :
 		def ts = (1..5).collect { createTestSuite(it)}
 		def itpi = (1..5).collect{ createItpi(it, ts) }
@@ -61,9 +63,9 @@ class CampaignDeletionHandlerImplTest  extends Specification{
 		result == []
 
 	}
-	
+
 	def "should simulate test suite deletion with errors"(){
-		
+
 		given :
 		def ts = (1..5).collect { createTestSuite(it)}
 		def itpi = (1..5).collect{ createItpi(it, ts) }
@@ -77,14 +79,14 @@ class CampaignDeletionHandlerImplTest  extends Specification{
 		result.size == 1
 
 	}
-	
+
 	def "should delete test suite "(){
-		
+
 		given :
 		def ts = (1..5).collect { createTestSuite(it)}
 		def itpi = (1..5).collect{ createItpi(it, ts) }
 		ts.each {it.getTestPlan() >> itpi}
-	
+
 		suiteDao.findAll(_) >> ts
 		def ids = [1L, 2L, 5L]
 		when :
@@ -94,14 +96,14 @@ class CampaignDeletionHandlerImplTest  extends Specification{
 
 	}
 
-	
+
 	def "should delete test suite and remove iteration test plan item from iteration "(){
-		
+
 		given :
 		def ts = (1..5).collect { createTestSuite(it)}
 		def itpi = (1..5).collect{ createItpi(it, ts) }
 		ts.each {it.getTestPlan() >> itpi}
-	
+
 		suiteDao.findAll(_) >> ts
 		def ids = [1L, 2L, 3L, 4L, 5L]
 		when :
@@ -110,14 +112,14 @@ class CampaignDeletionHandlerImplTest  extends Specification{
 		5 * iterationTestPlanManagerService.removeTestPlanFromIteration(_)
 
 	}
-	
+
 	def "should delete test suite and not remove iteration test plan item from iteration if the item is in another not deleted suite"(){
-		
+
 		given :
 		def ts = (1..5).collect { createTestSuite(it)}
 		def itpi = (1..5).collect{ createItpi(it, ts) }
 		ts.each {it.getTestPlan() >> itpi}
-	
+
 		suiteDao.findAll(_) >> ts
 		def ids = [1L, 3L, 4L, 5L]
 		when :
@@ -126,23 +128,23 @@ class CampaignDeletionHandlerImplTest  extends Specification{
 		0 * iterationTestPlanManagerService.removeTestPlanFromIteration(_)
 
 	}
-	
-	
-	
-	
-	
-	
+
+
+
+
+
+
 	def createTestSuite = {id -> TestSuite ts = Mock(TestSuite)
 		ts.getId() >> id
 		ts.getAttachmentList() >> []
 		ts.getIteration() >> []
 		return ts}
-	
+
 	def createItpi = {id, testSuites -> IterationTestPlanItem itpi = Mock(IterationTestPlanItem)
 		itpi.getId() >> id
 		itpi.getTestSuites() >> testSuites.collect{it}
 		itpi.getExecutions() >> []
 		return itpi}
-	
-	
+
+
 }
