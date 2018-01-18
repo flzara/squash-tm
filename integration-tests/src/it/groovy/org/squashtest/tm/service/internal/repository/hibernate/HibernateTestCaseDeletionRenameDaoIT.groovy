1a23fada243ad9c62c16c253a2073736f8ae46d7
jthebault <jthebault@bitbucket.org>
2018-01-18 14:35:14 +0100

    renaming a class because of a case error

--- integration-tests/src/it/groovy/org/squashtest/tm/service/internal/repository/hibernate/HibernateTestCaseDeletionRenameDaoIT.groovy
+++ integration-tests/src/it/groovy/org/squashtest/tm/service/internal/repository/hibernate/HibernateTestCaseDeletionRenameDaoIT.groovy
@@ -43,7 +43,7 @@ import javax.inject.Inject
 @NotThreadSafe
 @UnitilsSupport
 @Transactional
-class HibernateTestCaseDeletionDaoIT extends DbunitDaoSpecification{
+class HibernateTestCaseDeletionRenameDaoIT extends DbunitDaoSpecification{
 
 	@Inject
 	private TestCaseDeletionDao deletionDao;
