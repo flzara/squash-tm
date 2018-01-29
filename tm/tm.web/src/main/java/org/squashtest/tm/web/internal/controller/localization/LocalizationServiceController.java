1334dfee03d892102141a2170db181bac2cb7ffd
ChrisHolc <chris_holc@hotmail.fr>
2018-01-29 09:25:05 +0100

    XSS in localization

--- tm/tm.web/src/main/java/org/squashtest/tm/web/internal/controller/localization/LocalizationServiceController.java
+++ tm/tm.web/src/main/java/org/squashtest/tm/web/internal/controller/localization/LocalizationServiceController.java
@@ -1,28 +1,24 @@
 /**
- *     This file is part of the Squashtest platform.
- *     Copyright (C) Henix, henix.fr
- *
- *     See the NOTICE file distributed with this work for additional
- *     information regarding copyright ownership.
- *
- *     This is free software: you can redistribute it and/or modify
- *     it under the terms of the GNU Lesser General Public License as published by
- *     the Free Software Foundation, either version 3 of the License, or
- *     (at your option) any later version.
- *
- *     this software is distributed in the hope that it will be useful,
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *     GNU Lesser General Public License for more details.
- *
- *     You should have received a copy of the GNU Lesser General Public License
- *     along with this software.  If not, see <http://www.gnu.org/licenses/>.
+ * This file is part of the Squashtest platform.
+ * Copyright (C) Henix, henix.fr
+ * <p>
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ * <p>
+ * This is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * <p>
+ * this software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this software.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.squashtest.tm.web.internal.controller.localization;
-
-import java.util.Locale;
-
-import javax.inject.Inject;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -32,20 +28,22 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.squashtest.tm.web.internal.i18n.InternationalizationHelper;
 import org.squashtest.tm.web.internal.i18n.MessageObject;
 
+import javax.inject.Inject;
+import java.util.Locale;
 
+// XSS OK
 @Controller
 @RequestMapping("/localization")
 public class LocalizationServiceController {
-	
+
 	@Inject
 	private InternationalizationHelper i18nHelper;
 
-	@RequestMapping(value="/filler", method=RequestMethod.POST,  headers="Accept=application/json")
+	@RequestMapping(value = "/filler", method = RequestMethod.POST, headers = "Accept=application/json")
 	@ResponseBody
-	public MessageObject fillLocalizationObject(@RequestBody MessageObject messageObject, Locale locale){
+	public MessageObject fillLocalizationObject(@RequestBody MessageObject messageObject, Locale locale) {
 		i18nHelper.resolve(messageObject, locale);
 		return messageObject;
 	}
-	
-	
+
 }
