faa33974f7374b8f46eb66346f16983353562a02
jthebault <jthebault@bitbucket.org>
2018-03-29 17:48:28 +0200

    Gherkin editor in french

--- tm/tm.web/src/main/webapp/scripts/test-case-management/script-editor/ScriptEditorTab.js
+++ tm/tm.web/src/main/webapp/scripts/test-case-management/script-editor/ScriptEditorTab.js
@@ -18,7 +18,7 @@
  *     You should have received a copy of the GNU Lesser General Public License
  *     along with this software.  If not, see <http://www.gnu.org/licenses/>.
  */
-define(["jquery", "backbone", "underscore", "ace/ace", "workspace.routing"], function ($, Backbone, _, ace, urlBuilder) {
+define(["jquery", "backbone", "underscore", "ace/ace", "workspace.routing", "ace/ext-language_tools"], function ($, Backbone, _, ace, urlBuilder) {
 	var ScriptEditorTab = Backbone.View.extend({
 
 		el: "#tab-tc-script-editor",
@@ -32,9 +32,32 @@ define(["jquery", "backbone", "underscore", "ace/ace", "workspace.routing"], fun
 
 		_initializeEditor: function (serverModel) {
 			var editor = ace.edit("tc-script-editor");
+			editor.session.setValue(serverModel.scriptExender.script);
+			var line0 = editor.session.getLine(0);
+			var locale = "en";
+			console.log(line0);
+			if (line0 !== null) {
+				line0 = line0.trim();
+				if (line0.search("language:") !== -1) {
+					var parsedLocale = line0.substring(line0.length - 2);
+					console.log(parsedLocale);
+					if (parsedLocale === "fr") {
+						locale = "fr";
+					}
+				}
+			}
+
+			if (locale === "fr") {
+				editor.session.setMode("ace/mode/gherkin-fr");
+			} else {
+				editor.session.setMode("ace/mode/gherkin");
+			}
 			editor.setTheme("ace/theme/twilight");
-			editor.session.setMode("ace/mode/gherkin");
-			editor.session.setValue(serverModel.scriptExender.script)
+			editor.setOptions({
+				enableBasicAutocompletion: true,
+				enableSnippets: true,
+				enableLiveAutocompletion: true
+			});
 			this.editor = editor;
 		},
 
