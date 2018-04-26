ffae69958c12260c19c0a8d78bfd5e677e28444f
jthebault <jthebault@bitbucket.org>
2018-04-26 11:20:02 +0200

    adding lines to table snippets

--- tm/tm.web/src/main/webapp/scripts/lib/ace-editor/src/snippets/gherkin-fr.js
+++ tm/tm.web/src/main/webapp/scripts/lib/ace-editor/src/snippets/gherkin-fr.js
@@ -10,6 +10,8 @@ snippet scenario\n\
 		Soit ${2}\n\
 		Quand ${3}\n\
 		Alors ${4}\n\
+snippet param\n\
+	<${1}>\n\
 snippet plan_scenario\n\
   \n\
 	Plan du scénario: ${1}\n\
@@ -20,17 +22,21 @@ snippet plan_scenario\n\
 snippet tab3*2\n\
 	| ${1}\t| ${2}\t| ${3}\t|\n\
 	| ${4}\t| ${5}\t| ${6}\t|\n\
+	| ${7}\t| ${8}\t| ${9}\t|\n\
 snippet tab4*2\n\
 	| ${1}\t| ${2}\t| ${3}\t| ${4}\t|\n\
 	| ${5}\t| ${6}\t| ${7}\t| ${8}\t|\n\
+	| ${9}\t| ${10}\t| ${11}\t| ${12}\t|\n\
 snippet tab3*3\n\
 	| ${1}\t| ${2}\t| ${3}\t|\n\
 	| ${4}\t| ${5}\t| ${6}\t|\n\
 	| ${7}\t| ${8}\t| ${9}\t|\n\
+	| ${10}\t| ${11}\t| ${12}\t|\n\
 snippet tab4*3\n\
 	| ${1}\t| ${2}\t| ${3}\t| ${4}\t\n\
 	| ${5}\t| ${6}\t| ${7}\t| ${8}\t\n\
 	| ${9}\t| ${10}\t| ${11}\t| ${12}\t\n\
+	| ${13}\t| ${14}\t| ${15}\t| ${16}\t\n\
 ";
 // @formatter:on
 	console.log(exports.snippetText);
