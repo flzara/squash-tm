97f38dc429ad9e9ad7b7e84c0bb4a685d62e191f
jeremieNX <devnull@localhost>
2018-06-06 11:35:55 +0200

    [Issue 7469] Order Gherkin snippets

--- tm/tm.web/src/main/webapp/scripts/lib/ace-editor/src/snippets/gherkin-es.js
+++ tm/tm.web/src/main/webapp/scripts/lib/ace-editor/src/snippets/gherkin-es.js
@@ -4,39 +4,39 @@ define("ace/snippets/gherkin-es", ["require", "exports", "module"], function (re
 	// DO NOT REFORMAT THE SNIPPETS, INDENTATION MUST BE PRESERVED
 	exports.snippetText =
 "\
+snippet tab4*3\n\
+	| ${1}\t| ${2}\t| ${3}\t| ${4}\t|\n\
+	| ${5}\t| ${6}\t| ${7}\t| ${8}\t|\n\
+	| ${9}\t| ${10}\t| ${11}\t| ${12}\t|\n\
+	| ${13}\t| ${14}\t| ${15}\t| ${16}\t|\n\
+snippet tab3*3\n\
+	| ${1}\t| ${2}\t| ${3}\t|\n\
+	| ${4}\t| ${5}\t| ${6}\t|\n\
+	| ${7}\t| ${8}\t| ${9}\t|\n\
+	| ${10}\t| ${11}\t| ${12}\t|\n\
+snippet tab4*2\n\
+	| ${1}\t| ${2}\t| ${3}\t| ${4}\t|\n\
+	| ${5}\t| ${6}\t| ${7}\t| ${8}\t|\n\
+	| ${9}\t| ${10}\t| ${11}\t| ${12}\t|\n\
+snippet tab3*2\n\
+	| ${1}\t| ${2}\t| ${3}\t|\n\
+	| ${4}\t| ${5}\t| ${6}\t|\n\
+	| ${7}\t| ${8}\t| ${9}\t|\n\
+snippet esquema_del_escenario\n\
+	\n\
+	Esquema del escenario: ${1}\n\
+		Dado ${2}\n\
+		Cuando ${3}\n\
+		Entonces ${4}\n\
+		Ejemplos: ${5}\n\
 snippet escenario\n\
   \n\
 	Escenario: ${1}\n\
 		Dado ${2}\n\
 		Cuando ${3}\n\
 		Entonces ${4}\n\
-snippet esquema_del_escenario\n\
-  \n\
-	Esquema del escenario: ${1}\n\
-		Dado ${2}\n\
-		Cuando ${3}\n\
-		Entonces ${4}\n\
-		Ejemplos: ${5}\n\
 snippet param\n\
-		<${1}>\n\
-snippet tab3*2\n\
-	| ${1}\t| ${2}\t| ${3}\t|\n\
-	| ${4}\t| ${5}\t| ${6}\t|\n\
-	| ${7}\t| ${8}\t| ${9}\t|\n\
-snippet tab4*2\n\
-	| ${1}\t| ${2}\t| ${3}\t| ${4}\t|\n\
-	| ${5}\t| ${6}\t| ${7}\t| ${8}\t|\n\
-	| ${9}\t| ${10}\t| ${11}\t| ${12}\t|\n\
-snippet tab3*3\n\
-	| ${1}\t| ${2}\t| ${3}\t|\n\
-	| ${4}\t| ${5}\t| ${6}\t|\n\
-	| ${7}\t| ${8}\t| ${9}\t|\n\
-	| ${10}\t| ${11}\t| ${12}\t|\n\
-snippet tab4*3\n\
-	| ${1}\t| ${2}\t| ${3}\t| ${4}\t|\n\
-	| ${5}\t| ${6}\t| ${7}\t| ${8}\t|\n\
-	| ${9}\t| ${10}\t| ${11}\t| ${12}\t|\n\
-	| ${13}\t| ${14}\t| ${15}\t| ${16}\t|\n\
+	<${1}>\n\
 ";
 // @formatter:on
 	exports.scope = "gherkin-es";
--- tm/tm.web/src/main/webapp/scripts/lib/ace-editor/src/snippets/gherkin-fr.js
+++ tm/tm.web/src/main/webapp/scripts/lib/ace-editor/src/snippets/gherkin-fr.js
@@ -4,39 +4,39 @@ define("ace/snippets/gherkin-fr", ["require", "exports", "module"], function (re
 	// DO NOT REFORMAT THE SNIPPETS, INDENTATION MUST BE PRESERVED
 	exports.snippetText =
 "\
+snippet tab4*3\n\
+	| ${1}\t| ${2}\t| ${3}\t| ${4}\t|\n\
+	| ${5}\t| ${6}\t| ${7}\t| ${8}\t|\n\
+	| ${9}\t| ${10}\t| ${11}\t| ${12}\t|\n\
+	| ${13}\t| ${14}\t| ${15}\t| ${16}\t|\n\
+snippet tab3*3\n\
+	| ${1}\t| ${2}\t| ${3}\t|\n\
+	| ${4}\t| ${5}\t| ${6}\t|\n\
+	| ${7}\t| ${8}\t| ${9}\t|\n\
+	| ${10}\t| ${11}\t| ${12}\t|\n\
+snippet tab4*2\n\
+	| ${1}\t| ${2}\t| ${3}\t| ${4}\t|\n\
+	| ${5}\t| ${6}\t| ${7}\t| ${8}\t|\n\
+	| ${9}\t| ${10}\t| ${11}\t| ${12}\t|\n\
+snippet tab3*2\n\
+	| ${1}\t| ${2}\t| ${3}\t|\n\
+	| ${4}\t| ${5}\t| ${6}\t|\n\
+	| ${7}\t| ${8}\t| ${9}\t|\n\
+snippet plan_scenario\n\
+	\n\
+	Plan du scénario: ${1}\n\
+		Soit ${2}\n\
+		Quand ${3}\n\
+		Alors ${4}\n\
+		Exemples: ${5}\n\
 snippet scenario\n\
-  \n\
+	\n\
 	Scénario: ${1}\n\
 		Soit ${2}\n\
 		Quand ${3}\n\
 		Alors ${4}\n\
 snippet param\n\
 	<${1}>\n\
-snippet plan_scenario\n\
-  \n\
-	Plan du scénario: ${1}\n\
-		Soit ${2}\n\
-		Quand ${3}\n\
-		Alors ${4}\n\
-		Exemples: ${5}\n\
-snippet tab3*2\n\
-	| ${1}\t| ${2}\t| ${3}\t|\n\
-	| ${4}\t| ${5}\t| ${6}\t|\n\
-	| ${7}\t| ${8}\t| ${9}\t|\n\
-snippet tab4*2\n\
-	| ${1}\t| ${2}\t| ${3}\t| ${4}\t|\n\
-	| ${5}\t| ${6}\t| ${7}\t| ${8}\t|\n\
-	| ${9}\t| ${10}\t| ${11}\t| ${12}\t|\n\
-snippet tab3*3\n\
-	| ${1}\t| ${2}\t| ${3}\t|\n\
-	| ${4}\t| ${5}\t| ${6}\t|\n\
-	| ${7}\t| ${8}\t| ${9}\t|\n\
-	| ${10}\t| ${11}\t| ${12}\t|\n\
-snippet tab4*3\n\
-	| ${1}\t| ${2}\t| ${3}\t| ${4}\t|\n\
-	| ${5}\t| ${6}\t| ${7}\t| ${8}\t|\n\
-	| ${9}\t| ${10}\t| ${11}\t| ${12}\t|\n\
-	| ${13}\t| ${14}\t| ${15}\t| ${16}\t|\n\
 ";
 // @formatter:on
 	exports.scope = "gherkin-fr";
