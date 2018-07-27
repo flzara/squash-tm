fb0431268a5a534960897d22f1932f1f78a0d441
bflessel <devnull@localhost>
2018-07-27 19:02:59 +0200

    Fixing issues with CUF in Management folders

--- tm/tm.service/src/main/java/org/squashtest/tm/service/internal/repository/hibernate/package-info.java
+++ tm/tm.service/src/main/java/org/squashtest/tm/service/internal/repository/hibernate/package-info.java
@@ -760,7 +760,7 @@
 	@NamedQuery(name = "BoundEntityDao.findAllTestCaseFoldersIdsForProject", query = "select n.id from TestCaseLibraryNode n join n.project p where p.id = :projectId"),
 	@NamedQuery(name = "BoundEntityDao.findAllRequirementFoldersIdsForProject", query = "select n.id from RequirementLibraryNode n join n.project p where p.id = :projectId"),
 	@NamedQuery(name = "BoundEntityDao.findAllProjectIdsForProject", query = "select p.id from Project p where p.id = :projectId"),
-	@NamedQuery(name = "BoundEntityDao.findAllCustomReportFoldersIdsForProject", query = "select n.entityId from CustomReportLibraryNode n where n.library.project.id = :projectId"),
+	@NamedQuery(name = "BoundEntityDao.findAllCustomReportFoldersIdsForProject", query = "select n.entityId from CustomReportLibraryNode n where n.library.project.id = :projectId and n.entityType = 'FOLDER'"),
 	@NamedQuery(name = "BoundEntityDao.findCurrentCustomReportFoldersId", query = "select n.entityId from CustomReportLibraryNode n where n.id = :clnId and n.entityType = 'FOLDER'"),
 	@NamedQuery(name = "BoundEntityDao.findCurrentProjectFromCustomReportFoldersId", query = "select n.library.project.id from CustomReportLibraryNode n where n.id = :clnId and n.entityType = 'LIBRARY'"),
 
