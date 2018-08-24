51241823fbefb64ab0684d6003c3bdbdbfb474c6
Aubin Guilhem <devnull@localhost>
2018-08-24 10:44:57 +0200

    [SQTM-451] Preparation for full xampaign export optimization.

--- tm/tm.service/src/main/java/org/squashtest/tm/service/internal/dto/ExecutionStepDto.java
+++ tm/tm.service/src/main/java/org/squashtest/tm/service/internal/dto/ExecutionStepDto.java
@@ -20,14 +20,33 @@
  */
 package org.squashtest.tm.service.internal.dto;
 
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
 public class ExecutionStepDto {
 	private Long id;
 	private String status="";
+	private Integer stepOrder;
+	private String lastExecutedBy = "";
+	private Date lastExecutedOn;
+	private String comment = "";
+	private Long testStepId;
+	private Set<Long> requirementSet = new HashSet<>();
+	private Set<Long> issueSet = new HashSet<>();
 
 	public ExecutionStepDto(Long id, String status) {
 		super();
 		this.id = id;
 		this.status = status;
+	}
+
+	public ExecutionStepDto(Long id, String status, Integer stepOrder, Long testStepId) {
+		super();
+		this.id = id;
+		this.status = status;
+		this.stepOrder = stepOrder;
+		this.testStepId = testStepId;
 	}
 
 	public ExecutionStepDto(){
@@ -48,5 +67,69 @@ public class ExecutionStepDto {
 
 	public void setStatus(String status) {
 		this.status = status;
+	}
+
+	public Integer getStepOrder() {
+		return stepOrder;
+	}
+
+	public void setStepOrder(Integer stepOrder) {
+		this.stepOrder = stepOrder;
+	}
+
+	public String getLastExecutedBy() {
+		return lastExecutedBy;
+	}
+
+	public void setLastExecutedBy(String lastExecutedBy) {
+		this.lastExecutedBy = lastExecutedBy;
+	}
+
+	public Date getLastExecutedOn() {
+		return lastExecutedOn;
+	}
+
+	public void setLastExecutedOn(Date lastExecutedOn) {
+		this.lastExecutedOn = lastExecutedOn;
+	}
+
+	public String getComment() {
+		return comment;
+	}
+
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+
+	public Long getTestStepId() {
+		return testStepId;
+	}
+
+	public void setTestStepId(Long testStepId) {
+		this.testStepId = testStepId;
+	}
+
+	public Set<Long> getRequirementSet() {
+		return requirementSet;
+	}
+
+	public void setRequirementSet(Set<Long> requirementSet) {
+		this.requirementSet = requirementSet;
+	}
+
+	public void addRequirement(Long requirementId){
+		requirementSet.add(requirementId);
+	}
+
+	public Set<Long> getIssueSet() {
+		return issueSet;
+	}
+
+	public void setIssueSet(Set<Long> issueSet) {
+		this.issueSet = issueSet;
+	}
+
+	public void addIssue(Long issueId){
+		issueSet.add(issueId);
 	}
 }
