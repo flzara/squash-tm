59b58b72a9ffd573da2c249cc2c34aba9f78f6b2
johanNx <devnull@localhost>
2019-01-15 11:15:05 +0100

    [Scm] Trailing event for previous commit

--- tm/tm.web/src/main/webapp/scripts/scm-server/ScmRepositoriesTableView.js
+++ tm/tm.web/src/main/webapp/scripts/scm-server/ScmRepositoriesTableView.js
@@ -30,7 +30,6 @@ define(['jquery', 'backbone', 'squash.translator', './AddScmRepositoryDialog', '
 		events: {
 			"click #add-scm-repository" : "openAddScmRepositoryDialog",
 			"click #delete-scm-repositories" : "openDeleteMultipleRepositoriesDialog",
-			'click #scm-repository-table td.col-name' : 'openChangeNamePopup',
 			'click #scm-repository-table td.col-path' : 'openChangePathPopup',
 			'click #scm-repository-table td.col-folder' : 'openChangeFolderPopup',
 			'click #scm-repository-table td.col-branch' : 'openChangeBranchPopup'
