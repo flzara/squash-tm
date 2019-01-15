976c37d31b4812e749000a375eb58fbb8fec8b7c
aboittiaux <aboittiaux@henix.fr>
2019-01-15 17:06:58 +0100

    [Issue 7925] fixed

--- tm/tm.service/src/main/java/org/squashtest/tm/service/internal/testcase/CustomTestCaseModificationServiceImpl.java
+++ tm/tm.service/src/main/java/org/squashtest/tm/service/internal/testcase/CustomTestCaseModificationServiceImpl.java
@@ -1101,6 +1101,8 @@ public class CustomTestCaseModificationServiceImpl implements CustomTestCaseModi
 				createRequestForTestCase(testCaseId, null);
 			}
 		}
+		List<IterationTestPlanItem> iterationTestPlanItems = iterationTestPlanFinder.findByReferencedTestCase(tc);
+		reindexItpisReferencingTestCase(tc);
 		indexationService.reindexTestCase(testCaseId);
 		return tc.getProject().isAllowAutomationWorkflow();
 	}
