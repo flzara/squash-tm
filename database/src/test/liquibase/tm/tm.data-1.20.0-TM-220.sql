d819b788941fb82c3c1683c2b5d59a962711d66b
Aubin Guilhem <devnull@localhost>
2019-03-20 09:57:26 +0100

    [TM-224] Add missing authority to NOT delete.

--- database/src/test/liquibase/tm/tm.data-1.20.0-TM-220.sql
+++ database/src/test/liquibase/tm/tm.data-1.20.0-TM-220.sql
@@ -1,11 +1,13 @@
 insert into CORE_PARTY(PARTY_ID)
 values
 (-50),
-(-51);
+(-51),
+(-52);
 
 insert into CORE_PARTY_AUTHORITY(PARTY_ID, AUTHORITY)
 values
 (-50, 'ROLE_TM_USER'),
-(-51, 'ROLE_TM_USER');
+(-51, 'ROLE_TM_USER'),
+(-52, 'ROLE_TF_AUTOMATION_PROGRAMMER');
 
 
