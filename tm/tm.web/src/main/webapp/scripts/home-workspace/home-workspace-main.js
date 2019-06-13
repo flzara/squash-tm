814e4fa2c2270d431870fb24413a3ad44edcfa86
Aubin Guilhem <devnull@localhost>
2019-06-13 10:31:53 +0200

    Display message on home workspace

--- tm/tm.web/src/main/webapp/scripts/home-workspace/home-workspace-main.js
+++ tm/tm.web/src/main/webapp/scripts/home-workspace/home-workspace-main.js
@@ -18,13 +18,80 @@
  *     You should have received a copy of the GNU Lesser General Public License
  *     along with this software.  If not, see <http://www.gnu.org/licenses/>.
  */
-define(['./home-main-view'],
-    function (MainView) {
+define(['./home-main-view', 'jquery', 'squash.translator'],
+    function (MainView, $, translator) {
         "use strict";
 
          function init() {
+					  initInformationBlock();
          		new MainView();
          }
+
+         function initInformationBlock(){
+         	 var informationBlock = $("#information-block");
+         	 if (informationBlock != null){
+				var dateMessage = initDateMessage();
+				var userMessage = initUserMessage();
+				if(dateMessage != null){
+					appendMessage(dateMessage);
+				}
+				if(userMessage != null){
+					appendMessage(userMessage);
+				}
+			}
+		 }
+
+		 function initDateMessage() {
+         	var dateMessage;
+
+			var dateLicenseInformation = squashtm.app.dateLicenseInformation;
+
+			if(dateLicenseInformation !== null && dateLicenseInformation !== ''){
+				var daysRemaining = parseInt(dateLicenseInformation);
+				if(daysRemaining < 0) {
+					dateMessage = translator.get("information.expirationDate.warning3", getExpirationDate(daysRemaining));
+				} else if (daysRemaining < 30){
+					dateMessage = translator.get("information.expirationDate.warning2", getExpirationDate(daysRemaining));
+				} else if (daysRemaining < 61){
+					dateMessage = translator.get("information.expirationDate.warning1", getExpirationDate(daysRemaining));
+				}
+			}
+			return dateMessage;
+		 }
+
+		 function getExpirationDate(daysRemaining){
+         	var currentDate = new Date();
+         	var expirationDate = currentDate + daysRemaining;
+			return expirationDate.toLocaleDateString(undefined, {
+				day: '2-digit',
+				month: '2-digit',
+				year: 'numeric'
+				})
+		 }
+
+		 function initUserMessage() {
+			var userLicenseInformation = squashtm.app.userLicenseInformation;
+			var userLicenseInformationArray = userLicenseInformation.split("-");
+			var activeUsersCount = userLicenseInformationArray[0];
+			var maxUsersAllowed = userLicenseInformationArray[1];
+			var allowCreateUsers = JSON.parse(userLicenseInformationArray[2]);
+
+			var userMessage;
+			if (!allowCreateUsers){
+				userMessage = translator.get("information.userExcess.warning2", maxUsersAllowed, activeUsersCount);
+			} else {
+				userMessage = translator.get("information.userExcess.warning1", maxUsersAllowed, activeUsersCount);
+			}
+
+			return userMessage;
+		 }
+
+		 function appendMessage(message){
+         	var informationBlockContent = $("#information-block-content");
+         	var element = document.createElement("p");
+         	element.innerHTML = message;
+         	informationBlockContent.append(element);
+		 }
 
          return {
              init : init
