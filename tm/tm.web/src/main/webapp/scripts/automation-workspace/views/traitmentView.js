c0033e9ce308655c755c837d1fb4117284af5a75
jeremieNX <devnull@localhost>
2019-06-19 16:46:23 +0200

    [TM-485] Fixed

--- tm/tm.web/src/main/webapp/scripts/automation-workspace/views/traitmentView.js
+++ tm/tm.web/src/main/webapp/scripts/automation-workspace/views/traitmentView.js
@@ -32,7 +32,7 @@ define(["jquery", "underscore", "backbone", "handlebars", "squash.translator", '
                 var self = this;
                 var datatableSettings = {
                     sAjaxSource: squashtm.app.contextRoot + "automation-workspace/automation-requests/traitment",
-                    "aaSorting": [[7, 'desc'], [8, 'asc'], [10, 'desc']],
+                    "aaSorting": [[8, 'desc'], [9, 'asc'], [10, 'desc']],
                     "bDeferRender": true,
                     "iDisplayLength": 25,
                     "aoColumnDefs": [{
