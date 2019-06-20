518b169b838be5f3d7c687e9edafc78ca2935a0e
bsiri <devnull@localhost>
2019-06-20 11:34:09 +0200

    [TM-282] fixed test

--- tm/tm.service/src/test/groovy/org/squashtest/tm/service/internal/query/QueryPlannerTest.groovy
+++ tm/tm.service/src/test/groovy/org/squashtest/tm/service/internal/query/QueryPlannerTest.groovy
@@ -511,8 +511,8 @@ from TestCase testCase
 
 		then :
 		res.toString() == """select testCase.id
-from TestCase testCase, CustomFieldValue null_12
-where null_12.boundEntityType = ?1 and null_12.boundEntityId = testCase.id and null_12.cufId = ?2""";
+from TestCase testCase, CustomFieldValue TEST_CASE.value_12
+where TEST_CASE.value_12.boundEntityType = ?1 and TEST_CASE.value_12.boundEntityId = testCase.id and TEST_CASE.value_12.cufId = ?2""";
 	}
 
 
