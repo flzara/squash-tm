3e2d426e5d75b15dd7b0667e9a88d2cb8687b047
QuanHenix <devnull@localhost>
2019-12-12 10:29:40 +0100

    [TM-976] IE incompability with String.prototype.includes in ES6 fixed

--- tm/tm.web/src/main/webapp/scripts/tree/plugins/workspace-tree-plugin.js
+++ tm/tm.web/src/main/webapp/scripts/tree/plugins/workspace-tree-plugin.js
@@ -27,6 +27,20 @@ define(['jquery', 'underscore', 'workspace.tree-node-copier', 'workspace.permiss
 		"jquery.squash.oneshotdialog", 'workspace.projects', 'jstree'],
 	function ($, _, nodecopier, rulesbroker, translator, oneshot, projects) {
 
+		if (!String.prototype.includes) {
+			String.prototype.includes = function(search, start) {
+				'use strict';
+				if (typeof start !== 'number') {
+					start = 0;
+				}
+
+				if (start + search.length > this.length) {
+					return false;
+				} else {
+					return this.indexOf(search, start) !== -1;
+				}
+			};
+		}
 
 		/* *******************************************************************************
                       Library part
