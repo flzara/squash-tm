2c39ef06c897e122937f65bc89c26f6b6679718f
jprioux <jprioux@henix.fr>
2020-07-30 13:16:05 +0200

    removed useless import

--- tm/tm.web/src/main/java/org/squashtest/tm/web/internal/controller/testcase/steps/TestCaseTestStepsController.java
+++ tm/tm.web/src/main/java/org/squashtest/tm/web/internal/controller/testcase/steps/TestCaseTestStepsController.java
@@ -23,7 +23,6 @@ package org.squashtest.tm.web.internal.controller.testcase.steps;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BeanPropertyBindingResult;
