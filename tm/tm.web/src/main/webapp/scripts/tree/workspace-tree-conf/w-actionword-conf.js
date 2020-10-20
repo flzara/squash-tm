abf8a5109b58d194ff377e387010f50772e561de
johanNx <lor_johan@hotmail.com>
2020-10-20 17:17:37 +0200

    [SQUASH-1251] Copy an action word - Setup client side permissions

---
+++ tm/tm.web/src/main/webapp/scripts/tree/workspace-tree-conf/w-actionword-conf.js
@@ -0,0 +1,43 @@
+/*
+ *     This file is part of the Squashtest platform.
+ *     Copyright (C) Henix, henix.fr
+ *
+ *     See the NOTICE file distributed with this work for additional
+ *     information regarding copyright ownership.
+ *
+ *     This is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Lesser General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     this software is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Lesser General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Lesser General Public License
+ *     along with this software.  If not, see <http://www.gnu.org/licenses/>.
+ */
+define([], function() {
+	"use strict";
+	return {
+		generate : function(){
+
+			return {
+				"types" : {
+					"max_depth" : -2, // unlimited without check
+					"max_children" : -2, // unlimited w/o check
+					"valid_children" : ["drive"],
+					"types" : {
+						"action-word" : {
+							"valid_children" : 'none'
+						},
+						"drive" : {
+							"valid_children" : ["action-word"]
+						}
+					}
+				}
+			};
+		}
+	};
+});
